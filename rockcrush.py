cd4bdf745ef056b2fdd57751a7299dd3756e1498
Ali Ugur <aliugur997@gmail.com>
2024-10-29 17:08:10 +0300

    Feat(microk8s): Now it deletes Microk8s local registry images

--- rockcrush.py
+++ rockcrush.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 import subprocess
 
+def get_microk8s_image_list():
+    result = subprocess.run(['microk8s', 'ctr', 'images', 'ls'], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    images = output.splitlines()
+    rock_images = []
+    for image in images:
+        if 'localhost:32000' in image:
+            rock_images.append(image)
+    return rock_images
 
-def get_image_list():
+def get_docker_image_list():
     result = subprocess.run(['docker', 'images'], stdout=subprocess.PIPE)
     output = result.stdout.decode('utf-8')
     images = output.splitlines()
@@ -12,10 +21,18 @@ def get_image_list():
             rock_images.append(image)
     return rock_images
 
-def get_image_id(rock: str):
+def get_microk8s_image_id(rock: str):
+    return rock.split()[0]
+
+def get_docker_image_id(rock: str):
     return rock.split()[2]
 
-def remove_rock(rock: str):
+def remove_microk8s_rock(image_id: str):
+    print(f'removing image {image_id}')
+    result = subprocess.run(['microk8s', 'ctr', 'image', 'remove', image_id], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = result.stdout.decode('utf-8')
+
+def remove_docker_rock(rock: str):
     image_id = get_image_id(rock)
     print(f'removing image {image_id}')
     result = subprocess.run(['docker', 'rmi', '-f', image_id], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -29,16 +46,15 @@ def remove_rock(rock: str):
         output = result.stdout.decode('utf-8')
 
     print(output)
-
 if __name__ == '__main__':
     print('Current rocks')
-    rocks = get_image_list()
+    rocks = get_microk8s_image_list()
     for rock in rocks:
         print(rock)
-
     for rock in rocks:
-        remove_rock(rock)
+        remove_microk8s_rock(get_microk8s_image_id(rock))
+
     print('Final rocks')
-    rocks = get_image_list()
+    rocks = get_microk8s_image_list()
     for rock in rocks:
         print(rock)
